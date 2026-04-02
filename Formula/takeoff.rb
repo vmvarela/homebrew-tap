996fd2de84888a6028f9a242fc4cb82aef65a779
Victor M. Varela <vmvarela@gmail.com>
2026-04-02 16:21:46 +0200

    Update Takeoff to 0.3.0-1-g289affa [skip ci]

--- Formula/takeoff.rb
+++ Formula/takeoff.rb
@@ -1,8 +1,9 @@
 class Takeoff < Formula
   desc "Release automation for Zig projects"
   homepage "https://github.com/vmvarela/takeoff"
-  url "https://github.com/vmvarela/takeoff/releases/download/v0.3.0/takeoff-v0.3.0-macos-aarch64.tar.gz"
+  url "https://github.com/vmvarela/takeoff/releases/download/v0.3.0-1-g289affa/takeoff-v0.3.0-macos-aarch64.tar.gz"
   sha256 "2fb23ea2a3754a6e022c14433caa3b07a72bd9d81c66337bdb4c0f07e7eb7f58"
+  version "0.3.0-1-g289affa"
   license "MIT"
 
   head "https://github.com/vmvarela/takeoff.git"
